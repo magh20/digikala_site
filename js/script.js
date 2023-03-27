59cb329747674b7e4b816085e3f5ef17fa4fdf2b
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-03-27 13:46:50 +0330

    start jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    $(".menu ul li").hover(function(){
+        $(this).find("span i").removeClass("fa-caret-down");
+        $(this).find("span i").addClass("fa-caret-up");
+    }, 
+    function(){
+        $(this).find("span i").removeClass("fa-caret-up");
+        $(this).find("span i").addClass("fa-caret-down");
+    })
 });
