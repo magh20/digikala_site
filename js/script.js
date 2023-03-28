c7c27824f1422743163df4faa3340714cba22e01
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-03-28 21:29:57 +0330

    create sub-menu

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
     $(".menu ul li").hover(function(){
         $(this).find("span i").removeClass("fa-caret-down");
         $(this).find("span i").addClass("fa-caret-up");
+        $(".sub-menu").show();
+        $(this).find("ul").show();
     }, 
     function(){
         $(this).find("span i").removeClass("fa-caret-up");
         $(this).find("span i").addClass("fa-caret-down");
+        $(".sub-menu").css("display","none");
+        $(this).find("ul").css("display","none");
     })
 });
