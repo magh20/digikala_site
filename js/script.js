206e6cdd0a3227b50d062de682aa91fda6eb554f
mohammad amin ghorbani <63232995+magh20@users.noreply.github.com>
2023-03-29 14:38:28 +0330

    finish sub-menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
     $(".menu ul li").hover(function(){
-        $(this).find("span i").removeClass("fa-caret-down");
-        $(this).find("span i").addClass("fa-caret-up");
-        $(".sub-menu").show();
-        $(this).find("ul").show();
+        let countul = $(this).find("ul").length;
+        if(countul != 0){
+            $(this).find("span i").removeClass("fa-caret-down");
+            $(this).find("span i").addClass("fa-caret-up");
+            $(".sub-menu").show();
+            $(this).find("ul").delay().fadeIn();
+            //or use - $(".sub-menu").slideDown(""); -
+        }
     }, 
     function(){
         $(this).find("span i").removeClass("fa-caret-up");
         $(this).find("span i").addClass("fa-caret-down");
         $(".sub-menu").css("display","none");
-        $(this).find("ul").css("display","none");
+        $(".menu>ul>li").find("ul").hide();
     })
-});
+}); //menu
+
+$(document).ready(function(){ 
+    $(".slider1-righticon").click(function(){
+
+    })
+}); //slider1
